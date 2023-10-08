7c29e33a67f7cd39f48447f2a46cc00acdb86c10
ionut ciornei <Pantelimon194@gmail.com>
2023-10-08 20:49:02 +0300

    added horizontal line for corvinilor and transfagarasan components

--- src/components/Transfagarasan.tsx
+++ src/components/Transfagarasan.tsx
@@ -11,6 +11,7 @@ const Transfagarasan = () => {
             <div className='transf-description'>
             <p> The Transfăgărășan is a paved mountain road <br /> crossing the  southern  section  of the Carpathian <br /> Mountains of Romania. </p>
             <p> The road climbs to an altitude of 2,042 metres <br /> (6,699 ft), making it the second highest <br /> mountain pass in Romania after the Transalpina </p>  
+            <hr className='transf-horizontal-botttom'/>
             </div>
             <hr />
         </Container>
